32655f938b1cfa99fabe9a119782f2be87054e66
Julie Tang <julieanntang@hotmail.com>
2021-08-24 02:33:07 -0600

    post new note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,6 +1,27 @@
 class NotesController < ApplicationController
   def index
+    puts "index called"
     notes = Note.all
     render component: "Notes", props: { notes: notes }
+  end
+
+  def new
+    render component: "NewNote"
+  end
+
+  def create
+    # this creates a note in memory
+    note = Note.new(note_params)
+    if note.save
+      #go back to index page
+      redirect_to notes_path
+    else
+      # handle invalid input here
+    end
+  end
+
+  private
+  def note_params
+    params.require(:note).permit(:title, :description)
   end
 end
